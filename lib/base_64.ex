cb2f09c67e34c419b7fbfffde5ba1e6a0304776a
Chris Grieger <chris@scolp.de>
2014-08-19 08:01:43 +0200

    Implemented decoding.

--- lib/base_64.ex
+++ lib/base_64.ex
@@ -4,20 +4,20 @@ defmodule Solution.Base64 do
 
   ## Examples
 
-      iex> Solution.Base64.pad_bit_length "fo", 6
+      iex> Solution.Base64.pad_bits "fo", 6
       <<102, 111, 0::size(2)>>
 
   """
-  def pad_bit_length(b, to_multiple_of) when rem(bit_size(b), to_multiple_of) == 0 do
+  def pad_bits(b, to_multiple_of) when rem(bit_size(b), to_multiple_of) == 0 do
       b
   end
 
-  def pad_bit_length(b, to_multiple_of) do    
+  def pad_bits(b, to_multiple_of) do    
     current_bits = bit_size(b)
     factor = div(current_bits, to_multiple_of)  
     remainder = (to_multiple_of * factor + to_multiple_of) - current_bits
 
-    pad_bit_length(<<b::binary, 0::size(remainder)>>, to_multiple_of)
+    pad_bits(<<b::binary, 0::size(remainder)>>, to_multiple_of)
   end
 
 
@@ -40,7 +40,7 @@ defmodule Solution.Base64 do
     end
 
     # Pad the input with 0-bits so we can run the base64 algo    
-    padded = pad_bit_length(input, 6)    
+    padded = pad_bits(input, 6)    
     base64_encode(padded, "")
   end
   
@@ -61,31 +61,61 @@ defmodule Solution.Base64 do
     base64_encode(<<b::size(remaining_bits)>>, acc <> Map.get(Constants.base64_map, codepoint))
   end
 
+  @doc ~S"""
+  Decode the input string or charlist from base64 to a utf8 string. This function always returns a string.
+
+  ## Examples
+
+      iex> Solution.Base64.decode_string "Zm9v"
+      "foo"
+
+      iex> Solution.Base64.decode_string 'YmFy'
+      "bar"
+
+  """
   def decode_string(input) do    
     # Convert the input to a string if it is a char list
     if is_list(input) do
       input = to_string input
     end
     
-    # Pad the input with 0-bits so we can run the base64 algo    
     base64_decode(input, <<>>)
   end
 
   defp base64_decode(b, acc) when bit_size(b) == 0  do
-    four_characters_to_three_bytes(acc)
+    # Pad to 4 bytes
+    acc = pad_bits(acc, 32)
+
+    four_characters_to_three_bytes(acc, "")
   end
 
   defp base64_decode(b, acc) do
     <<character, b::binary>> = b
-
-    byte = Map.get(Constants.reverse_base64_map, <<character>>)
+    
+    # Revert the base64 conversion
+    byte = Map.get(Constants.reverse_base64_map, <<character>>)    
     base64_decode(b, acc <> <<byte>>)
   end
 
-  defp four_characters_to_three_bytes(chars) do 
-    _window = 8
+  defp four_characters_to_three_bytes(chars, acc) when bit_size(chars) == 0 do
+    acc
+  end
 
-    <<chars::size(1), rest::binary>> = <<chars::binary>>
-    chars
+  defp four_characters_to_three_bytes(chars, acc) do 
+    _window = 6
+    _pad = 2
+
+    <<_::size(_pad), b1::size(_window), 
+    _::size(_pad), b2::size(_window), 
+    _::size(_pad), b3::size(_window), 
+    _::size(_pad), b4::size(_window), chars::bits>> = chars
+
+    <<decoded_bytes::bits>> = <<b1::size(_window), b2::size(_window), b3::size(_window), b4::size(_window)>>
+    
+    case decoded_bytes do
+      <<x, 0, 0>> -> four_characters_to_three_bytes(chars, acc <> <<x>>)
+      <<x, y, 0>> -> four_characters_to_three_bytes(chars, acc <> <<x, y>>)
+      x -> four_characters_to_three_bytes(chars, acc <> decoded_bytes)
+    end     
   end
 end
--- test/base_64_test.exs
+++ test/base_64_test.exs
@@ -3,13 +3,13 @@ defmodule Solution.Base64Test do
   doctest Solution.Base64
   import Solution.Base64
 
-  test "pad_bit_length - size is ok" do
-    padded = pad_bit_length(<<0, 0, 0, 1>>, 6)
+  test "pad_bits - size is ok" do
+    padded = pad_bits(<<0, 0, 0, 1>>, 6)
     assert bit_size(padded) == 36
   end
 
-  test "pad_bit_length - needs padding" do
-    padded = pad_bit_length(<<1, 1>>, 6)
+  test "pad_bits - needs padding" do
+    padded = pad_bits(<<1, 1>>, 6)
 
     # Check the correct size
     assert bit_size(padded) == 18
@@ -17,17 +17,8 @@ defmodule Solution.Base64Test do
     assert padded == <<1,1, 0::1, 0::1>>
   end
 
-  test "pad_bit_length - pad string" do
-    padded = pad_bit_length("ab", 6)
-    
-    # Check the correct size
-    assert bit_size(padded) == 18
-    # Check the correct value
-    assert padded == <<97, 98, 0::1, 0::1>>
-  end
-
-  test "pad_bit_length - pad string" do
-    padded = pad_bit_length("ab", 6)
+  test "pad_bits - pad string" do
+    padded = pad_bits("ab", 6)
     
     # Check the correct size
     assert bit_size(padded) == 18
@@ -55,7 +46,23 @@ defmodule Solution.Base64Test do
     assert "Zm9v" == encode_string 'foo'
   end
 
-  test "decode base64 string" do 
+  test "decode base64 string - no padding" do 
     assert "foo" == decode_string "Zm9v"
+  end
+
+  test "decode base64 string" do 
+    assert "b" == decode_string "Yg"
+  end
+
+  test "decode base64 string - unicode" do
+    assert "ł" = decode_string "xYI"
+  end
+
+  test "encode and decode lorem ipsum" do
+    input = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+    encoded = encode_string input
+    decoded = decode_string encoded
+
+    assert decoded == input
   end
 end
